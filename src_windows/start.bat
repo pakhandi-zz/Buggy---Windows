5f8f71647ef3cb9244930038fc51316c788a1b96
pakhandi <asimkprasad@gmail.com>
2015-03-05 22:40:29 +0530

    BugFix : V.2.1

--- src_windows/start.bat
+++ src_windows/start.bat
@@ -2,8 +2,12 @@
 @echo %round%>temp.txt
 @cd C:/CF/dist
 @ini.exe<temp.txt
-@subl ./%round%
-@cd C:/CF/dist/%round%/A/
-@subl aprog.cpp
-@start http://codeforces.com/contest/%round%/problems
-@exit
+IF EXIST %round% (
+ @subl ./%round%
+ @cd C:/CF/dist/%round%/A/
+ @subl aprog.cpp
+ @start http://codeforces.com/contest/%round%/problems
+ @exit
+) ELSE (
+exit
+)
